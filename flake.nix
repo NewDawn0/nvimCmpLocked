fc8558192500390cd92fed795313104c3a28fa9c
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-04 22:21:36 +0100

    switch to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -3,30 +3,31 @@
 
   inputs = {
     utils.url = "github:NewDawn0/nixUtils";
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs, utils, ... }: {
     overlays.default = final: prev: {
       vimPlugins = prev.vimPlugins // {
-        cmp-locked = self.packages.${prev.system}.default;
+        nvim-cmp = self.packages.${prev.system}.nvim-cmp;
+        cmp-buffer = self.packages.${prev.system}.cmp-buffer;
+        cmp-nvim-lsp = self.packages.${prev.system}.cmp-nvim-lsp;
+        cmp-path = self.packages.${prev.system}.cmp-path;
+        cmp_luasnip = self.packages.${prev.system}.cmp_luasnip;
+        lspkind-nvim = self.packages.${prev.system}.lspkind-nvim;
+        luasnip = self.packages.${prev.system}.luasnip;
+        nvim-lspconfig = self.packages.${prev.system}.nvim-lspconfig;
       };
     };
     packages = utils.lib.eachSystem { inherit nixpkgs; } (pkgs: {
-      default = pkgs.buildEnv {
-        name = "cmp-locked";
-        paths = with pkgs.vimPlugins; [
-          cmp-buffer
-          cmp-nvim-lsp
-          cmp-path
-          cmp_luasnip
-          lspkind-nvim
-          luasnip
-          nvim-cmp
-          nvim-lspconfig
-        ];
-        pathsToLink = [ "/share/vim-plugins" ];
-      };
+      nvim-cmp = pkgs.vimPlugins.nvim-cmp;
+      cmp-buffer = pkgs.vimPlugins.cmp-buffer;
+      cmp-nvim-lsp = pkgs.vimPlugins.cmp-nvim-lsp;
+      cmp-path = pkgs.vimPlugins.cmp-path;
+      cmp_luasnip = pkgs.vimPlugins.cmp_luasnip;
+      lspkind-nvim = pkgs.vimPlugins.lspkind-nvim;
+      luasnip = pkgs.vimPlugins.luasnip;
+      nvim-lspconfig = pkgs.vimPlugins.nvim-lspconfig;
     });
   };
 }

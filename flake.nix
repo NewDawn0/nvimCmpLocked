026f386740ec42b1f91892b1f00c72d25ccf6c67
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-05-07 00:24:47 +1000

    udpate locked versions

--- flake.nix
+++ flake.nix
@@ -1,7 +1,13 @@
 {
   description = "Locked versions of nvim-cmp & dependencies";
 
-  inputs.utils.url = "github:NewDawn0/nixUtils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils = {
+      url = "github:NewDawn0/nixUtils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = { self, utils, ... }: {
     overlays.default = final: prev: {
